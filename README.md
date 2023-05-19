c0860b693eb1db01d61ec380c7f393a0a4cf6878
Craig Osterhout <craig.osterhout@docker.com>
2023-05-19 07:44:41 -0700

    update llama syntax and use pinecone
    Signed-off-by: Craig Osterhout <craig.osterhout@docker.com>

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
+This repo may or may not work. I use it to experiment with different things and to keep a checkpoint.
+
 1. Clone this repo.
 
-2. Create `.env` file with your `OPENAI_API_KEY`.
+2. Create `.env` file with your `OPENAI_API_KEY` and `PINECONE_API_KEY`.
    You're responsible for any credit usage. This app is not optimized to minimize credit usage.
 
-3.Run the app and ask questions.
+3. A lot of things may or may not be commented out depending on what I was testing. Look at what's commented and maybe uncomment it.
+
+4. To run the app, use the following:
    ```
    docker compose run --build --rm app
    ```
--- app.py
+++ app.py
@@ -2,14 +2,16 @@ import json
 import requests
 from bs4 import BeautifulSoup
 from llama_index import (
-     GPTSimpleVectorIndex,
+     GPTVectorStoreIndex,
      ServiceContext,
      LLMPredictor,
      PromptHelper,
      Document,
      download_loader
-)
+     )
+from llama_index.vector_stores import PineconeVectorStore
 from llama_index.node_parser import SimpleNodeParser
+from llama_index.storage.storage_context import StorageContext
 from langchain import OpenAI
 import urllib.request
 import xml.etree.ElementTree as ET
@@ -17,92 +19,67 @@ import os
 import sys
 from datetime import datetime
 import time
+import pinecone
+
 
 
 def load_documents_to_gpt_vectorstore(urls):
     BeautifulSoupWebReader = download_loader("BeautifulSoupWebReader")
     loader = BeautifulSoupWebReader()
-    documents = []
-    for url in urls:
-        url_array = []
-        url_array.append(url)
-        documents.append(loader.load_data(url_array))
-        print(url)
-        time.sleep(5) # add a delay of 1 second between loading webpages
+    documents = loader.load_data(urls)
     parser = SimpleNodeParser()
-
     nodes = parser.get_nodes_from_documents(documents)
-    max_input_size = 4096
+    max_input_size = 1024
     num_output = 256
     max_chunk_overlap = 20
+    chunk_size_limit = 1000
     api_key = os.environ.get('OPENAI_API_KEY')
     llm_predictor = LLMPredictor(
         llm=OpenAI(
-            temperature=0, model_name="text-davinci-003", openai_api_key=api_key
+            temperature=0, model_name="embedding-ada-002", openai_api_key=api_key
         )
     )
 
-    prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
+    prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap, chunk_size_limit)
 
     service_context = ServiceContext.from_defaults(
         llm_predictor=llm_predictor, prompt_helper=prompt_helper
     )
-    index = GPTSimpleVectorIndex(nodes, service_context=service_context)
-    index.save_to_disk("/usr/src/app/data/gpt_index_docs.json")
-    return index
 
+    environment="us-west1-gcp-free"
+    index_name = "docker-docs-index"
+    dimension=1536
+    pinecone.init(api_key=os.environ.get('PINECONE_API_KEY'), environment=environment)
+    if index_name in pinecone.list_indexes():
+        # Delete the index
+        pinecone.delete_index(index_name)
+    pinecone.create_index(index_name, metric="cosine", dimension=dimension)
+    pinecone_index = pinecone.Index(index_name)
+    vector_store = PineconeVectorStore(
+        pinecone_index=pinecone_index,
+        environment=environment
+    )
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+    index = GPTVectorStoreIndex.from_documents(documents, storage_context=storage_context, service_context=service_context)
 
-def scrape(site):
-    urls = []
-    def scrape_helper(current_site):
-        nonlocal urls
-
-        r = requests.get(current_site)
-
-        s = BeautifulSoup(r.text, "html.parser")
-        #print(s.find_all("a"))
-        for i in s.find_all("a"):
-                if "href" in i.attrs:
-                    href = i.attrs["href"]
-
-                    if href.startswith("/") or href.startswith("#"):
-                            full_url = site + href
-
-                            print(full_url)
-                            if full_url not in urls:
-                                 urls.append(full_url)
-                                 scrape_helper(full_url)
-    scrape_helper(site)
-    return urls
 
 
 
 def get_sitemap_urls(site, modified_after=None):
-
     response = urllib.request.urlopen(site)
     sitemap_xml = response.read()
 
     root = ET.fromstring(sitemap_xml)
 
     urls = []
+    for url_elem in root.iter('{http://www.sitemaps.org/schemas/sitemap/0.9}url'):
+        loc_elem = url_elem.find('{http://www.sitemaps.org/schemas/sitemap/0.9}loc')
+        lastmod_elem = url_elem.find('{http://www.sitemaps.org/schemas/sitemap/0.9}lastmod')
+        lastmod_str = lastmod_elem.text if lastmod_elem is not None else ""
+        lastmod = datetime.fromisoformat(lastmod_elem.text.rstrip('Z'))
 
-    for sitemap_elem in root.iter('{http://www.sitemaps.org/schemas/sitemap/0.9}sitemap'):
-        sitemap_url_elem = sitemap_elem.find('{http://www.sitemaps.org/schemas/sitemap/0.9}loc')
-        sitemap_url = sitemap_url_elem.text
-
-        response = urllib.request.urlopen(sitemap_url)
-        sitemap_xml = response.read()
-
-        sitemap_root = ET.fromstring(sitemap_xml)
-
-        for url_elem in sitemap_root.iter('{http://www.sitemaps.org/schemas/sitemap/0.9}url'):
-            loc_elem = url_elem.find('{http://www.sitemaps.org/schemas/sitemap/0.9}loc')
-            lastmod_elem = url_elem.find('{http://www.sitemaps.org/schemas/sitemap/0.9}lastmod')
-            lastmod_str = lastmod_elem.text if lastmod_elem is not None else ""
-            lastmod = datetime.fromisoformat(lastmod_elem.text.rstrip('Z'))
-
-            if modified_after is None or lastmod >= datetime.fromisoformat(modified_after):
-                urls.append(loc_elem.text)
+        if modified_after is None or lastmod >= datetime.fromisoformat(modified_after):
+            urls.append(loc_elem.text)
 
     return urls
 
@@ -117,14 +94,30 @@ def chat():
 
 
 def get_answer(query):
-    index = GPTSimpleVectorIndex.load_from_disk("/usr/src/app/data/gpt_index_docs.json")
-    response = index.query(query, similarity_top_k=10)
+    environment="us-west1-gcp-free"
+    index_name = "docker-docs-index"
+    dimension=1536
+    pinecone.init(api_key=os.environ.get('PINECONE_API_KEY'), environment=environment)
+    pinecone_index = pinecone.Index(index_name)
+    vector_store = PineconeVectorStore(
+        pinecone_index=pinecone_index,
+        environment=environment
+    )
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+    index = GPTVectorStoreIndex([], storage_context=storage_context)
+
+    query_engine = index.as_query_engine(similarity_top_k=10)
+    response = query_engine.query(query)
+
+    #response = index.query(query, similarity_top_k=10)
     print(response)
     for node in response.source_nodes:
         url = node.node.extra_info.get('URL')
         if url:
             print(url)
     return response
+
+
 
 
 def array_to_file(arr, file):
@@ -146,28 +139,21 @@ def file_to_array(file):
 def build_index_sitemap(site, url_file, modified_after=None):
     sitemap= site + "/sitemap.xml"
     url_array = get_sitemap_urls(sitemap, modified_after)
-    array_to_file(url_array, url_file)
+    #array_to_file(url_array, url_file)
 
     urls = file_to_array(url_file)
     load_documents_to_gpt_vectorstore(urls)
 
 
-def build_index_scrape(site, url_file):
-    url_array = scrape(site)
-    array_to_file(url_array, url_file)
-
-    #urls = file_to_array(url_file)
-    #load_documents_to_gpt_vectorstore(urls)
-
 
 def main():
-#   sites = ["https://docs.docker.com", "https://forums.docker.com"]
-#   data_folder = "/usr/src/app/data/"
-#   indices = ["urls.txt", "urls2.txt"]
+    site = "https://docs.docker.com"
+    data_folder = "/usr/src/app/data/"
+    indices = "urls.txt"
+    indices_path = data_folder + indices
 
-   #chat()
-#   build_index_sitemap("https://forums.docker.com", "/usr/src/app/data/urls2.txt", '2022-01-01')
-
+    build_index_sitemap(site, indices_path)
+    #chat()
 
 if __name__ == "__main__":
     main()
